b93583dee775610819401632072b38be1d17b538
Ananya Kharche <106643078+narneon@users.noreply.github.com>
2026-05-10 18:27:52 -0400

    add bq_test.py to test functionality of local_dev_sa

---
+++ src/Scripts/bq_test.py
@@ -0,0 +1,17 @@
+from google.cloud import bigquery
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../../data/local_dev_sa.json"
+client = bigquery.Client()
+
+# Initialize client
+client = bigquery.Client()
+
+# List datasets in the project
+datasets = list(client.list_datasets())
+if datasets:
+    print("Datasets accessible by the service account:")
+    for d in datasets:
+        print(f" - {d.dataset_id}")
+else:
+    print("No datasets found.")
